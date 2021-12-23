17aa2fecca003669a7ccf5796a5c80c1ddad17d1
ChaitanyaSaiKT <chaitanya.sai@thoughtfocus.com>
2021-12-23 13:03:55 +0530

    assignment exceptions

---
+++ exception/ArithmeticExceptions.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class ArithmeticExceptions {
+	public static void main(String[] args) {
+		int a=10,b=0;
+		int c=a/b;
+		System.out.println(+c);
+	}
+
+}
---
+++ exception/ArrayIndexOutofBoundException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class ArrayIndexOutofBoundException {
+	public static void main(String[] args) {
+		int data[]=new int[15];
+		data[16]=55;
+		
+		
+	}
+
+}
