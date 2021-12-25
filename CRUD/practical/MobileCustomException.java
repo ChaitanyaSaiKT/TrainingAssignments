830235eaf54093c687f1460fb3bba21cb79c495e
ChaitanyaSaiKT <chaitanya.sai@thoughtfocus.com>
2021-12-25 16:29:40 +0530

    CRUD Practical

---
+++ CRUD/practical/MobileCustomException.java
@@ -0,0 +1,9 @@
+package com.ThoughFocus.arrays.practical;
+
+public class MobileCustomException extends Exception{
+	@Override
+	public String getMessage() {
+		return "InvalidBrandName";
+	}
+	}
+	
