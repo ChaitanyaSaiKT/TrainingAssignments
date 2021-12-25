066c4ef94a1a34e970b70366c7b51505382453e1
ChaitanyaSaiKT <chaitanya.sai@thoughtfocus.com>
2021-12-25 15:46:42 +0530

    Practical CRUD Operations

---
+++ practical/practical CRUD Operation/MobileDTO.java
@@ -0,0 +1,79 @@
+package com.ThoughFocus.arrays.practical;
+
+public class MobileDTO {
+
+	private int mobileid;
+	private String modelname;
+	private long price;
+	private String memory;
+	private String brandname;
+	private long serialnumber;
+	private int noofcameras;
+	
+	public MobileDTO(){
+		
+	}
+
+	public int getMobileid() {
+		return mobileid;
+	}
+
+	public void setMobileid(int mobileid) {
+		this.mobileid = mobileid;
+	}
+
+	public String getModelname() {
+		return modelname;
+	}
+
+	public void setModelname(String modelname) {
+		this.modelname = modelname;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public void setPrice(long price) {
+		this.price = price;
+	}
+
+	public String getMemory() {
+		return memory;
+	}
+
+	public void setMemory(String memory) {
+		this.memory = memory;
+	}
+
+	public String getBrandname() {
+		return brandname;
+	}
+
+	public void setBrandname(String brandname) {
+		this.brandname = brandname;
+	}
+
+	public long getSerialnumber() {
+		return serialnumber;
+	}
+
+	public void setSerialnumber(long serialnumber) {
+		this.serialnumber = serialnumber;
+	}
+
+	public int getNoofcameras() {
+		return noofcameras;
+	}
+
+	public void setNoofcameras(int noofcameras) {
+		this.noofcameras = noofcameras;
+	}
+
+	@Override
+	public String toString() {
+		return "Mobile [mobileid=" + mobileid + ", modelname=" + modelname + ", price=" + price + ", memory=" + memory
+				+ ", brandname=" + brandname + ", serialnumber=" + serialnumber + ", noofcameras=" + noofcameras + "]";
+	}
+	
+}
---
+++ practical/practical CRUD Operation/MobileMethods.java
@@ -0,0 +1,7 @@
+package com.ThoughFocus.arrays.practical;
+
+public interface MobileMethods {
+	void getAll();
+	void getAllByBrandName(String brandName) throws MobileCustomException;
+
+}
